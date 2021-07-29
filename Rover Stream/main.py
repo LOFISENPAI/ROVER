85ccfb80cbf7cd3fa893b2b3b9e2a4ebe5a4753c
LOFISENPAI <LOFISENPAI@users.noreply.github.com>
2021-07-29 13:18:21 -0700

    update main.py
    fixed rover actuator to prevent pan/tilt from activating

--- Rover Stream/main.py
+++ Rover Stream/main.py
@@ -4,7 +4,7 @@
 # main.py
 # import the necessary packages
 from flask import Flask, render_template, Response, request
-from appCam_edit import Camera
+from appCam import VideoCamera
 from gpiozero import Servo, Robot
 from gpiozero.pins.pigpio import PiGPIOFactory
 from gpiozero import Servo
@@ -14,84 +14,84 @@ import threading
 import os
 import curses
 
-pi_camera = Camera(flip=False) # flip pi camera if upside down.
+pi_camera = VideoCamera(flip=False) # flip pi camera if upside down.
 
-# MOTOR A - LEFT SIDE GPIO CONSTANTS
-PWML = 21           # PWMA - H-bridge enable pin
-FL = 16             # AI1 - forward drive
-RL = 20             # AI2 - reverse drive
-# MOTOR B - RIGHT SIDE GPIO CONSTANTS
-PWMR = 5            # PWMB - H-bridge enable pin
-FR = 19             # BI1 - forward drive
-RR = 13             # BI2 - reverse drive
-# OTHER SENSORS
-#ECHO = 23           # echo pin on distance sensor
-#TRIG = 24           # trigger pin on distance sensor
-#RDISK = 17          # encoder disk on the right rear wheel
-PANPIN = 25         # pan servo
-TILTPIN = 27        # tilt servo
+#Define Motor Driver GPIO pins
+# Motor A, LEFT
+PWML = 21     # PWMA - H-Bridge enable pin
+FL = 16   # AI1 - Forward Drive
+RL = 20   # AI2 - Reverse Drive
+# Motor B, RIGHT
+PWMR = 5     # PWMB - H-Bridge enable pin
+FR = 19  # BI1 - Forward Drive
+RR = 13  # BI2 - Reverse Drive
+# PAN AND TILT PINS
+PANPIN = 25
+TILTPIN = 27
 
-#set pin factory for servos and create objects of the Servo class
+# SET PIN FACTORY FOR SERVOS
 ping_fac = PiGPIOFactory()
 pan = Servo(PANPIN, pin_factory = ping_fac)
-tilt = Servo(TILTPIN,pin_factory = ping_fac)
+tilt = Servo(TILTPIN, pin_factory = ping_fac)
 
-# rover controls
+# ROVER
 rover = Robot((FL,RL,PWML), (FR,RR,PWMR))
 
-# FWD/BACK SPEED, TURN SPEED, CURVE SPEED
+# MOTOR SPEED
 FSPD = 0.5
 BSPD = 0.6
 TSPD = 0.7
 CSPD = 1.0
 
-# APP GLOBALS (DO NOT EDIT)
+# App Globals (do not edit)
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    """Video streaming home page."""
-    return render_template('index.html')
-    
+    return render_template('index.html') 
+
 def gen(camera):
+    #get camera frame
     while True:
         frame = camera.get_frame()
         yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-               
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
 @app.route('/video_feed')
 def video_feed():
-    return Response(gen(Camera()),
+    return Response(gen(pi_camera),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
+
 
 @app.route("/<deviceName>/<action>")
 def action(deviceName, action):
-    # DEVICES
-    if deviceName == 'pan':
+    if deviceName == "pan":
         actuator = pan
-    if deviceName == 'tilt':
+    if deviceName == "tilt":
         actuator = tilt
-    if deviceName == 'rover':
+    if deviceName == "rover":
         actuator = rover
-    # ACTIONS
     if action == "max":
         if actuator.value < 1:
             actuator.value += 0.2
+        sleep(0.005)
     if action == "min":
         if actuator.value > -1:
             actuator.value -= 0.2
+        sleep(0.005)
     if action == "mid":
         actuator.mid()
+        sleep(0.005)
     if action == "forward":
-        actuator.forward(FSPD)
+        rover.forward(FSPD)
     if action == "backward":
-        actuator.backward(BSPD)
+        rover.backward(BSPD)
     if action == "stop":
-        actuator.stop()
+        rover.stop()
     if action == "left":
-        actuator.left(TSPD)
+        rover.left(TSPD)
     if action == "right":
-        actuator.right(TSPD)
+        rover.right(TSPD)
     return render_template('index.html')
 
 if __name__ == '__main__':
