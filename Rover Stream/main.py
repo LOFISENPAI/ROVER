952ed4c89dc9feb7d7d29be1f87421d57d85cb25
LOFISENPAI <LOFISENPAI@users.noreply.github.com>
2021-07-29 13:03:45 -0700

    update
    Added camera controls and fixed website layout

--- Rover Stream/main.py
+++ Rover Stream/main.py
@@ -4,146 +4,94 @@
 # main.py
 # import the necessary packages
 from flask import Flask, render_template, Response, request
-from appCam import VideoCamera
-from gpiozero import PWMOutputDevice
-from gpiozero import DigitalOutputDevice
+from appCam_edit import Camera
+from gpiozero import Servo, Robot
+from gpiozero.pins.pigpio import PiGPIOFactory
+from gpiozero import Servo
+from time import sleep
 import time
 import threading
 import os
 import curses
 
-pi_camera = VideoCamera(flip=False) # flip pi camera if upside down.
+pi_camera = Camera(flip=False) # flip pi camera if upside down.
 
-#Define Motor Driver GPIO pins
-# Motor A, LEFT
-PWM_DRIVE_LEFT = 21     # PWMA - H-Bridge enable pin
-FORWARD_LEFT_PIN = 16   # AI1 - Forward Drive
-REVERSE_LEFT_PIN = 20   # AI2 - Reverse Drive
-# Motor B, RIGHT
-PWM_DRIVE_RIGHT = 5     # PWMB - H-Bridge enable pin
-FORWARD_RIGHT_PIN = 19  # BI1 - Forward Drive
-REVERSE_RIGHT_PIN = 13  # BI2 - Reverse Drive
+# MOTOR A - LEFT SIDE GPIO CONSTANTS
+PWML = 21           # PWMA - H-bridge enable pin
+FL = 16             # AI1 - forward drive
+RL = 20             # AI2 - reverse drive
+# MOTOR B - RIGHT SIDE GPIO CONSTANTS
+PWMR = 5            # PWMB - H-bridge enable pin
+FR = 19             # BI1 - forward drive
+RR = 13             # BI2 - reverse drive
+# OTHER SENSORS
+#ECHO = 23           # echo pin on distance sensor
+#TRIG = 24           # trigger pin on distance sensor
+#RDISK = 17          # encoder disk on the right rear wheel
+PANPIN = 25         # pan servo
+TILTPIN = 27        # tilt servo
 
-# Initialise objects for H-Bridge GPIO PWM pins
-# Use defaults for initial duty cycle - 0 and frequency=100Hz
-driveLeft = PWMOutputDevice(PWM_DRIVE_LEFT)
-driveRight = PWMOutputDevice(PWM_DRIVE_RIGHT)
+#set pin factory for servos and create objects of the Servo class
+ping_fac = PiGPIOFactory()
+pan = Servo(PANPIN, pin_factory = ping_fac)
+tilt = Servo(TILTPIN,pin_factory = ping_fac)
 
-# Initialise objects for H-Bridge digital GPIO pins
-forwardLeft = DigitalOutputDevice(FORWARD_LEFT_PIN)
-reverseLeft = DigitalOutputDevice(REVERSE_LEFT_PIN)
-forwardRight = DigitalOutputDevice(FORWARD_RIGHT_PIN)
-reverseRight = DigitalOutputDevice(REVERSE_RIGHT_PIN)
+# rover controls
+rover = Robot((FL,RL,PWML), (FR,RR,PWMR))
 
-# App Globals (do not edit)
+# FWD/BACK SPEED, TURN SPEED, CURVE SPEED
+FSPD = 0.5
+BSPD = 0.6
+TSPD = 0.7
+CSPD = 1.0
+
+# APP GLOBALS (DO NOT EDIT)
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html') 
-
+    """Video streaming home page."""
+    return render_template('index.html')
+    
 def gen(camera):
-    #get camera frame
     while True:
         frame = camera.get_frame()
         yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
-
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+               
 @app.route('/video_feed')
 def video_feed():
-    return Response(gen(pi_camera),
+    return Response(gen(Camera()),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
-@app.route('/allStop')
-def allStop():
-    forwardLeft.off()
-    reverseLeft.off()
-    forwardRight.off()
-    reverseRight.off()
-    driveLeft.value = 0
-    driveRight.value = 0
-    return render_template('index.html')
-
-@app.route('/forwardDrive')
-def forwardDrive():
-    forwardLeft.on()
-    reverseLeft.off()
-    forwardRight.on()
-    reverseRight.off()
-    driveLeft.value = 0.5
-    driveRight.value = 0.5
-    return render_template('index.html')
-
-@app.route('/reverseDrive')
-def reverseDrive():
-    forwardLeft.off()
-    reverseLeft.on()
-    forwardRight.off()
-    reverseRight.on()
-    driveLeft.value = 0.6
-    driveRight.value = 0.6
-    return render_template('index.html')
-
-@app.route('/spinLeft')
-def spinLeft():
-    forwardLeft.off()
-    reverseLeft.on()
-    forwardRight.on()
-    reverseRight.off()
-    driveLeft.value = 0.2
-    driveRight.value = 0.7
-    return render_template('index.html')
-
-@app.route('/spinRight')
-def spinRight():
-    forwardLeft.on()
-    reverseLeft.off()
-    forwardRight.off()
-    reverseRight.on()
-    driveLeft.value = 0.7
-    driveRight.value = 0.2
-    return render_template('index.html')
-
-@app.route('/backLeft')
-def backLeft():
-    forwardLeft.off()
-    reverseLeft.on()
-    forwardRight.off()
-    reverseRight.on()
-    driveLeft.value = 0.2
-    driveRight.value = 0.6
-    return render_template('index.html')
-
-@app.route('/backRight')
-def backRight():
-    forwardLeft.off()
-    reverseLeft.on()
-    forwardRight.off()
-    reverseRight.on()
-    driveLeft.value = 0.6
-    driveRight.value = 0.2
-    return render_template('index.html')
-
-@app.route("/<action>")
-def action(action):
-    if action == "forwardDrive":
-        forwardDrive()
-        allStop()
-    if action == "reverseDrive":
-        reverseDrive()
-        allStop()
-    if action == "spinLeft":
-        spinLeft()
-        allStop()
-    if action == "spinRight":
-        spinRight()
-        allStop()
-    if action == "backLeft":
-        backLeft()
-        allStop()
-    if action == "backRight":
-        backRight()
-        allStop() 
+@app.route("/<deviceName>/<action>")
+def action(deviceName, action):
+    # DEVICES
+    if deviceName == 'pan':
+        actuator = pan
+    if deviceName == 'tilt':
+        actuator = tilt
+    if deviceName == 'rover':
+        actuator = rover
+    # ACTIONS
+    if action == "max":
+        if actuator.value < 1:
+            actuator.value += 0.2
+    if action == "min":
+        if actuator.value > -1:
+            actuator.value -= 0.2
+    if action == "mid":
+        actuator.mid()
+    if action == "forward":
+        actuator.forward(FSPD)
+    if action == "backward":
+        actuator.backward(BSPD)
+    if action == "stop":
+        actuator.stop()
+    if action == "left":
+        actuator.left(TSPD)
+    if action == "right":
+        actuator.right(TSPD)
     return render_template('index.html')
 
 if __name__ == '__main__':
